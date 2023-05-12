3f4aa7fe9830df28b4c1a96f932d0d48e3551a36
Ciaran <cmd1@hw.ac.uk>
2023-05-12 18:04:09 +0100

    adding tinyGPT changes

--- tinyGPT.py
+++ tinyGPT.py
@@ -1,26 +1,16 @@
 # A small Python program for interfacing with the OpenAI API.
 # See https://platform.openai.com/docs/api-reference/chat
-import openai
 import os
 import sys
 import time
 import re
-
-# *Messages* are dict-pairs of strings, where the "role" part of the pair
-# indicates whether the message is from the user, system, or assistant.
-    # msg = {role : "user" | "system" | "assistant",  content : str}
-# A *conversation* a list of messages.
-# A *chat* is a dict-pair of a name and a conversation.
-# chat = {name : str, conv : msg list }.
+import openai
 
 history = [
   {"name": "Foo", "conv": []}, 
   {"name": "Bar", "conv": []}, 
   {"name": "Baz", "conv": []}
 ]
-
-def print_hist():
-  print("\n".join( f"{i} : {chat['name']}" for (i,chat) in zip(range(len(history)), history)))
 
 # A *configuration* is a dict-triple of a string, a chat, and a system message. 
 default_config = {
@@ -57,24 +47,6 @@ def resp_stoud_md(gen):
 def resp_str(gen):
   return ''.join(gen)
 
-def resp_file(path, n, gen):
-    # Read existing content of file
-    with open(path, "r") as f:
-        lines = f.readlines()
-
-    # Write modified content back to file
-    with open(path, "w") as f:
-        # Write prompt and timestamp before GPT response
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        f.writelines(lines[:n] + [f"\n*- BEGIN GPT at {timestamp} -*"])
-
-        # Write GPT response
-        for resp_frag in gen:
-            f.write(resp_frag)
-
-        # Write delimiter after GPT response
-        f.writelines(["*- END GPT -*\n\n"] + lines[n + 1:])
-
 ## Conversation management.
 def add_msg(conv, role, msg):
   conv.append({"role": role, "content": msg})
@@ -83,13 +55,18 @@ def chat(cfg, resp_handler, msg):
   add_msg(cfg["chat"]["conv"], "user", msg)
   resp_handler(call_gpt(cfg))
 
-def chat_file(cfg, path):
-  with open(path, "r") as f:
-    lines = f.read()
-    match = re.compile(r"\*-([\s\S]*?)-\*").search(lines)
-    prompt = match.group(1)
-    n = lines.count("\n", 0, match.start()) + 1
-  chat(cfg, lambda g : resp_file(path, n, g), prompt)
+ch = lambda x : chat(default_config, resp_stoud_md, x)
+
+# def resp_file(cfg, path, msg)
+#   with open(path, 'rw+'):
+
+# def chat_file(cfg, path):
+#   with open(path, "r") as f:
+#     lines = f.read()
+#     match = re.compile(r"\*-([\s\S]*?)-\*").search(lines)
+#     prompt = match.group(1)
+#     n = lines.count("\n", 0, match.start()) + 1
+#   chat(cfg, lambda g : resp_file(path, n, g), prompt)
 
 # Hack for sublimetext syntax markdown highlighting
 sys.ps1 = '''```python
